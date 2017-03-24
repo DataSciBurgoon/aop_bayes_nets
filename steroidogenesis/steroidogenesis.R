ff8bd765121b1c49d38e412b4e79e335f3c4c39e
Lyle D. Burgoon <lyle.d.burgoon@usace.army.mil>
2017-03-24 14:57:43 -0400

    created a new input file for the toxcast data. ran the bayes net for steroidogenesis. results are in the toxcast_steroidogenesis_results.RData file.

--- steroidogenesis/steroidogenesis.R
+++ steroidogenesis/steroidogenesis.R
@@ -135,12 +135,112 @@ met_results <- querygrain(setEvidence(bn_test, evidence=list(hydroxyprogesterone
 met_results_out <- format_results(met_results)
 write.table(met_results_out, file="metconazole_results.txt", sep="\t", row.names=TRUE, col.names=NA)
 
+format_results <- function(x){
+  xdf <- data.frame(matrix(unlist(x), nrow=length(x), byrow=T))
+  rownames(xdf) <- names(x)
+  colnames(xdf) <- c("yes", "no")
+  return(xdf)
+}
 
 
+format_evidence_list <- function(x){
+  evidence_list <- NULL
+  for(i in 1:nrow(x)){
+    evidence_list[[as.character(x[i,2])]] <- as.character(x[i,3])
+  }
+  return(as.list(evidence_list))
+}
 
+steroidogenesis_bn <- function(evidence){
+  evidence_list <- format_evidence_list(evidence)
+  yn <- c("yes", "no")
+  cholesterol <- cptable(~ cholesterol, values=c(0.95, 0.05), levels=yn)
+  cyp11a1 <- cptable(~ cyp11a1, values=c(0.50, 0.50), levels=yn)
+  pregnenolone <- cptable(~ pregnenolone | cyp11a1+cholesterol, values=c(0.99, 0.01, 0.01, 0.01,
+                                                                         0.01, 0.99, 0.99, 0.99), levels=yn)
+  cyp17a1_hydroxylase <- cptable(~ cyp17a1_hydroxylase, values=c(0.50, 0.50), levels=yn)
+  cyp17a1_lyase <- cptable(~ cyp17a1_lyase, values=c(.50, 0.50), levels=yn)
+  hsd3b1 <- cptable(~ hsd3b1, values=c(0.50, 0.50), levels=yn)
+  hsd17b3 <- cptable(~ hsd17b3, values=c(0.50, 0.50), levels=yn)
+  cyp19a1 <- cptable(~ cyp19a1, values=c(0.50, 0.50), levels=yn)
+  cyp21a1 <- cptable(~ cyp21a1, values=c(0.50, 0.50), levels=yn)
+  cyp11b1 <- cptable(~ cyp11b1, values=c(0.50, 0.50), levels=yn)
+  hydroxypregnenolone <- cptable(~ hydroxypregnenolone | cyp17a1_hydroxylase+pregnenolone, 
+                                 values=c(0.99, 0.01, 0.01, 0.99,
+                                          0.01, 0.99, 0.01, 0.99), 
+                                 levels=yn)
+  dhea <- cptable(~ dhea | cyp17a1_lyase+hydroxypregnenolone,
+                  values = c(0.99, 0.01, 0.01, 0.99,
+                             0.01, 0.99, 0.01, 0.99),
+                  levels=yn)
+  progesterone <- cptable(~ progesterone | hsd3b1+pregnenolone,
+                          values = c(0.99, 0.01, 0.01, 0.99,
+                                     0.01, 0.99, 0.01, 0.99),
+                          levels=yn)
+  hydroxyprogesterone <- cptable(~hydroxyprogesterone | cyp17a1_hydroxylase+progesterone,
+                                 values = c(0.99, 0.01, 0.01, 0.99,
+                                            0.01, 0.99, 0.01, 0.99),
+                                 levels=yn)
+  # androsteinedione <- cptable(~ androsteinedione | hsd3b1+dhea+cyp17a1_lyase+hydroxyprogesterone,
+  #                             values = c(0.99, 0.50, 0.50, 0.50, 0.50, 0.01, 0.01, 0.01, 0.50, 0.01, 0.01, 0.01, 0.50, 0.01, 0.01, 0.01,
+  #                                        0.01, 0.50, 0.50, 0.50, 0.50, 0.99, 0.99, 0.99, 0.50, 0.99, 0.99, 0.99, 0.50, 0.99, 0.99, 0.99),
+  #                             levels=yn)
+  androsteinedione1 <- cptable(~ androsteinedione1 | hsd3b1+dhea,
+                               values = c(0.99, 0.01, 0.01, 0.99,
+                                          0.01, 0.99, 0.01, 0.99),
+                               levels=yn)
+  androsteinedione2 <- cptable(~ androsteinedione2 | cyp17a1_lyase+hydroxyprogesterone,
+                               values = c(0.99, 0.01, 0.01, 0.99,
+                                          0.01, 0.99, 0.01, 0.99),
+                               levels=yn)
+  androsteinedione <- cptable(~ androsteinedione | androsteinedione1+androsteinedione2,
+                              values = c(0.99, 0.01, 0.99, 0.01,
+                                         0.99, 0.01, 0.01, 0.99),
+                              levels=yn)
+  estrone <- cptable(~ estrone | cyp19a1+androsteinedione,
+                     values = c(0.99, 0.01, 0.01, 0.99,
+                                0.01, 0.99, 0.01, 0.99),
+                     levels=yn)
+  testosterone <- cptable(~ testosterone | hsd17b3+androsteinedione,
+                          values=c(0.99, 0.01, 0.01, 0.99,
+                                   0.01, 0.99, 0.01, 0.99),
+                          levels=yn)
+  estradiol <- cptable(~ estradiol | cyp19a1+testosterone,
+                       values=c(0.99, 0.01, 0.01, 0.99,
+                                0.01, 0.99, 0.01, 0.99),
+                       levels=yn)
+  deoxycortisone <- cptable(~ deoxycortisone | cyp21a1+progesterone,
+                            values=c(0.99, 0.01, 0.01, 0.99,
+                                     0.01, 0.99, 0.01, 0.99),
+                            levels=yn)
+  corticosterone <- cptable(~ corticosterone | cyp11b1+deoxycortisone,
+                            values=c(0.99, 0.01, 0.01, 0.99,
+                                     0.01, 0.99, 0.01, 0.99),
+                            levels=yn)
+  deoxycortisol <- cptable(~ deoxycortisol | cyp21a1+hydroxyprogesterone,
+                           values=c(0.99, 0.01, 0.01, 0.99,
+                                    0.01, 0.99, 0.01, 0.99),
+                           levels=yn)
+  cortisol <- cptable(~ cortisol | cyp11b1+deoxycortisol,
+                      values=c(0.99, 0.01, 0.01, 0.99,
+                               0.01, 0.99, 0.01, 0.99),
+                      levels=yn)
+  
+  
+  plist <- compileCPT(list(cholesterol, cyp11a1, pregnenolone, cyp17a1_hydroxylase, cyp17a1_lyase, cyp21a1, cyp11b1, hsd3b1, hsd17b3, cyp19a1, hydroxypregnenolone,
+                           dhea, progesterone, hydroxyprogesterone, androsteinedione1, androsteinedione2, androsteinedione, estrone, testosterone, estradiol,
+                           deoxycortisone, corticosterone, deoxycortisol, cortisol))
+  bn_test <- grain(plist)
+  bn_query <- querygrain(setEvidence(bn_test, evidence=evidence_list))
+  bn_results <- format_results(bn_query)
+  return(bn_results)
+}
 
+toxcast_data <- read.table("toxcast_data.txt", sep="\t", header=TRUE)
+toxcast_data_split <- split(toxcast_data, f=toxcast_data$chemical_name)
+toxcast_steroidogenesis_results <- lapply(toxcast_data_split, steroidogenesis_bn)
 
-
+save(toxcast_steroidogenesis_results, file="toxcast_steroidogenesis_results.RData")
 
 
 
