d1174ec129ab9396bbf82ec8ca9608b49f2f2f80
Lyle D. Burgoon <lyle.d.burgoon@usace.army.mil>
2017-04-26 22:20:53 -0400

    Took a new analysis direction on the steatosis bayes net to demonstrate how to use it in an analysis.

--- steatosis/steatosis_bayes_net.R
+++ steatosis/steatosis_bayes_net.R
@@ -13,8 +13,8 @@ ppar_alpha <- cptable(~ ppara | fxr+shp+lxr,
 hsd17b4 <- cptable(~ hsd17b4 | ppara, values=c(0.95, 0.05, 0.05, 0.95), levels=yn)
 fatty_acid_beta_oxidation <- cptable(~ fatty_acid_beta_oxidation | hsd17b4,
                                      values = c(0.99, 0.01, 0.01, 0.99), levels=yn)
-steatosis <-  cptable(~steatosis | cytosolic_fatty_acids, 
-                      values=c(0.99, 0.01, 0.01, 0.99), levels=yn)
+steatosis <-  cptable(~steatosis | cytosolic_fatty_acids + fatty_acid_beta_oxidation, 
+                      values=c(0.01, 0.99, 0.01, 0.99, 0.99, 0.01, 0.99, 0.01), levels=yn)
 lrh1 <- cptable(~lrh1 | shp,
                 values=c(0.05, 0.95, 0.95, 0.05), levels=yn)
 pi3k <- cptable(~pi3k, values=c(0.50, 0.50), levels=yn)
@@ -38,9 +38,8 @@ scd1 <- cptable(~scd1 | srebp1, values=c(1, 0, 0, 1), levels=yn)
 lipogenesis <- cptable(~lipogenesis | scd1 + fas, values=c(0.95, 0.05, 0.05, 0.95,
                                                            0.95, 0.05, 0.05, 0.95),
                        levels=yn)
-cytosolic_fatty_acids <- cptable(~cytosolic_fatty_acids | lipogenesis + lfabp + fatty_acid_beta_oxidation, 
-                                 values=c(0.01, 0.99, 0.01, 0.99, 0.01, 0.99, 0.01, 0.99,
-                                          0.99, 0.01, 0.66, 0.34, 0.66, 0.34, 0.50, 0.50),
+cytosolic_fatty_acids <- cptable(~cytosolic_fatty_acids | lipogenesis + lfabp, 
+                                 values=c(0.99, 0.01, 0.99, 0.01, 0.99, 0.01, 0.01, 0.99),
                                  levels=yn)
 
 plist <- compileCPT(list(nrf2, fxr, shp, lxr, ppar_alpha, hsd17b4, 
@@ -48,8 +47,11 @@ plist <- compileCPT(list(nrf2, fxr, shp, lxr, ppar_alpha, hsd17b4,
                          mtorc2, ir, akt, lfabp, pparg, fas, mtorc1, apkc, srebp1, scd1, lipogenesis,
                          cytosolic_fatty_acids))
 bn_test <- grain(plist)
+png("steatosis_bn.png", height=6000, width=6000, res=300, pointsize=20)
 plot(bn_test)
-querygrain(setEvidence(bn_test, evidence=list(hsd17b4="no")))
+dev.off()
+querygrain(setEvidence(bn_test, evidence=list(fatty_acid_beta_oxidation="no")))
+querygrain(setEvidence(bn_test, evidence=list(cytosolic_fatty_acids="yes")))
 querygrain(setEvidence(bn_test, evidence=list(nrf2="no")))
 querygrain(setEvidence(bn_test, evidence=list(nrf2="yes")))
 querygrain(setEvidence(bn_test, evidence=list(hsd17b4="yes")))
